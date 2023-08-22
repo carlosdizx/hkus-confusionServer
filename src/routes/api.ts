c84a75ddf7eabc2eeeadcc3ee99ad8da2301d2cd
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-08-22 11:24:02 -0500

    :sparkles: create routes and implements in app

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,48 +4,50 @@ import jetValidator from 'jet-validator';
 import Paths from './constants/Paths';
 import User from '@src/models/User';
 import UserRoutes from './UserRoutes';
-
+import dishRouter from '@src/routes/dish.router';
+import promotionRouter from '@src/routes/promotion.router';
+import leaderRouter from '@src/routes/leader.router';
 
 // **** Variables **** //
 
 const apiRouter = Router(),
   validate = jetValidator();
 
-
 // ** Add UserRouter ** //
 
 const userRouter = Router();
 
 // Get all users
-userRouter.get(
-  Paths.Users.Get,
-  UserRoutes.getAll,
-);
+userRouter.get(Paths.Users.Get, UserRoutes.getAll);
 
 // Add one user
 userRouter.post(
   Paths.Users.Add,
   validate(['user', User.isUser]),
-  UserRoutes.add,
+  UserRoutes.add
 );
 
 // Update one user
 userRouter.put(
   Paths.Users.Update,
   validate(['user', User.isUser]),
-  UserRoutes.update,
+  UserRoutes.update
 );
 
 // Delete one user
 userRouter.delete(
   Paths.Users.Delete,
   validate(['id', 'number', 'params']),
-  UserRoutes.delete,
+  UserRoutes.delete
 );
 
 // Add UserRouter
 apiRouter.use(Paths.Users.Base, userRouter);
 
+//Add paths
+apiRouter.use('/dishes', dishRouter);
+apiRouter.use('/promotions', promotionRouter);
+apiRouter.use('/leaders', leaderRouter);
 
 // **** Export default **** //
 
